51e3f996d0eea1f2068555abb629bbcf08fe5392
João Lucas de oliveira Lopes <jlucaso@hotmail.com>
2022-09-13 10:48:56 -0300

    chore: add docker-compose for nextjs dev

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,7 @@ const Home: NextPage = () => {
   const title = isAuthenticated ? `Welcome ${username}` : "Please sign in";
   const action = isAuthenticated ? () => signOut() : () => signIn();
   const buttonText = isAuthenticated ? "Sign out" : "Sign in";
+  const buttonType = isAuthenticated ? "btn-error" : "btn-primary";
 
   if (isLoading) {
     return <div></div>;
@@ -33,7 +34,7 @@ const Home: NextPage = () => {
       )}
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center gap-y-5">
         <h1 className="text-5xl">{title}</h1>
-        <button className="btn max-w-max" onClick={action}>
+        <button className={`btn max-w-max ${buttonType}`} onClick={action}>
           {buttonText}
         </button>
       </div>
